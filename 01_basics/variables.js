d51c90c950c16129c4ab2e37f651c418d76fd0ae
PREET8612 <140526217+PREET8612@users.noreply.github.com>
2024-02-27 18:15:29 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+let accountId = 1234
+let accountEmail = "preet@gmail.com"
+let accountCity = "Surat"
+let accountName
+
+accountId = 6789
+accountEmail = "p@gmail.com"
+accountCity = "anand"
+console.table([accountId , accountEmail , accountCity , accountName])
