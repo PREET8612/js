030c23461d86e19ab770e4f7b15bba5e3e620c13
PREET8612 <140526217+PREET8612@users.noreply.github.com>
2024-02-27 18:21:22 +0000

    typesof

---
+++ 01_basics/typesof.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js as newer version
+
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+let IsloggedIn = "preet"
+
+let booleanIsloggedIn = Boolean(IsloggedIn)
+
+console.log(booleanIsloggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber)
